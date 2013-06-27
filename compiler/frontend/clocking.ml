9a432c6acd01e71c0df45f64f56aa7c66d58feec
Adrien Guatto <adrien.guatto@laposte.net>
2013-06-27 18:20:11 +0200

    Annotate static and clock exps with stream types not clock types

--- compiler/frontend/clocking.ml
+++ compiler/frontend/clocking.ml
@@ -68,7 +68,7 @@ type env =
   {
     intf_env : Interface.env;
     local_nodes : Clock_types.clock_sig Names.ShortEnv.t;
-    mutable ck_vars : VarTy.t Utils.Int_map.t;
+    mutable ck_vars : VarTySt.t Utils.Int_map.t;
   }
 
 let initial_env intf_env =
@@ -83,7 +83,7 @@ let reset_env env = { env with ck_vars = Utils.Int_map.empty; }
 let find_ck_var env v =
   try Utils.Int_map.find v env.ck_vars
   with Not_found ->
-    let t = fresh_ty () in
+    let t = fresh_st () in
     env.ck_vars <- Utils.Int_map.add v t env.ck_vars;
     t
 
@@ -181,10 +181,13 @@ let rec psplit_length pt =
 
 let ty_of_st st = Pct_stream st
 
-let on_ty loc t ce (ctx, constrs) =
+let st_of_ty loc t (ctx, constrs) =
   let st = fresh_st () in
-  ty_of_st (Pst_on (st, ce)),
-  (ctx, { loc = loc; desc = Tc_equal (ty_of_st st, t); } :: constrs)
+  st, (ctx, { loc = loc; desc = Tc_equal (ty_of_st st, t); } :: constrs)
+
+let on_ty loc t ce acc =
+  let st, acc = st_of_ty loc t acc in
+  ty_of_st (Pst_on (st, ce)), acc
 
 let adaptable_tys loc (ctx, constrs) =
   let st = fresh_st () in
@@ -211,12 +214,14 @@ module A =
 struct
   type new_annot =
     | Exp of VarTy.t
+    | ClockExp of VarTySt.t
     | Node of Clock_types.clock_sig
     | App of (int * VarTySt.t) list
 
   let print_new_annot fmt na =
     match na with
     | Exp ty -> VarTy.print fmt ty
+    | ClockExp st -> VarTySt.print fmt st
     | Node csig ->
       Clock_types.print_sig fmt csig
     | App inst ->
@@ -232,6 +237,9 @@ module M = Acids.Make(ANN_INFO)
 
 let annotate_exp info ty =
   ANN_INFO.annotate info (A.Exp ty)
+
+let annotate_clock_exp info st =
+  ANN_INFO.annotate info (A.ClockExp st)
 
 let annotate_node info csig =
   ANN_INFO.annotate info (A.Node csig)
@@ -253,41 +261,33 @@ struct
 
   let update_clock_exp_info { new_annot = na; old_annot = info; } =
     match na with
-    | Node _ | App _ -> invalid_arg "update_clock_exp_info"
-    | Exp pty ->
+    | Node _ | App _ | Exp _ -> invalid_arg "update_clock_exp_info"
+    | ClockExp pst ->
       (
-        let ty = Clock_types.ty_of_pre_ty pty in
-        match ty with
-        | Ct_stream st ->
-          object
-            method ci_data = info#ci_data
-            method ci_interv = info#ci_interv
-            method ci_static = info#ci_static
-            method ci_clock = st
-          end
-        | _ -> invalid_arg "update_clock_exp_info"
+        object
+          method ci_data = info#ci_data
+          method ci_interv = info#ci_interv
+          method ci_static = info#ci_static
+          method ci_clock = Clock_types.st_of_pre_st pst
+        end
       )
 
   let update_static_exp_info { new_annot = na; old_annot = info; } =
     match na with
-    | Node _ | App _ -> invalid_arg "update_static_exp_info"
-    | Exp pty ->
+    | Node _ | App _ | Exp _ -> invalid_arg "update_static_exp_info"
+    | ClockExp pst ->
       (
-        let ty = Clock_types.ty_of_pre_ty pty in
-        match ty with
-        | Ct_stream st ->
-          object
-            method pwi_data = info#pwi_data
-            method pwi_interv = info#pwi_interv
-            method pwi_static = info#pwi_static
-            method pwi_clock = st
+        object
+          method pwi_data = info#pwi_data
+          method pwi_interv = info#pwi_interv
+          method pwi_static = info#pwi_static
+          method pwi_clock = Clock_types.st_of_pre_st pst
           end
-        | _ -> invalid_arg "update_static_exp_info"
       )
 
   let update_exp_info { new_annot = na; old_annot = info; } =
     match na with
-    | Node _ | App _ -> invalid_arg "update_clock_exp_info"
+    | Node _ | App _ | ClockExp _ -> invalid_arg "update_exp_info"
     | Exp pty ->
       object
         method ei_data = info#ei_data
@@ -298,7 +298,7 @@ struct
 
   let update_app_info { new_annot = na; old_annot = (); } =
     match na with
-    | Exp _ | Node _ -> invalid_arg "update_app_info"
+    | Exp _ | Node _ | ClockExp _ -> invalid_arg "update_app_info"
     | App inst ->
       let trad (i, pst) = i, Clock_types.st_of_pre_st pst in
       {
@@ -309,7 +309,7 @@ struct
 
   let update_pat_info { new_annot = na; old_annot = info; } =
     match na with
-    | Node _ | App _ -> invalid_arg "update_clock_exp_info"
+    | Node _ | App _ | ClockExp _ -> invalid_arg "update_pat_info"
     | Exp pty ->
       let ty = ty_of_pre_ty pty in
       object
@@ -325,7 +325,7 @@ struct
 
   let update_node_info  { new_annot = na; old_annot = info; } =
     match na with
-    | Exp _ | App _ -> invalid_arg "update_node_info"
+    | Exp _ | App _ | ClockExp _ -> invalid_arg "update_node_info"
     | Node csig ->
       object
         method ni_ctx = info#ni_ctx
@@ -347,56 +347,59 @@ let rec clock_var v (ctx, constrs) =
 
 and clock_clock_exp env ce acc =
   let loc = ce.ce_loc in
-  let ced, ty, acc =
+  let ced, st, acc =
     match ce.ce_desc with
     | Ce_var v ->
       let ty, acc = clock_var v acc in
-      M.Ce_var v, ty, acc
+      let st, acc = st_of_ty loc ty acc in
+      M.Ce_var v, st, acc
 
     | Ce_pword pw ->
-      let ty = fresh_ty () in
+      let st = fresh_st () in
       let pw, acc =
-        let expect = expect_static_exp ty in
+        let expect = expect_static_exp (ty_of_st st) in
         Ast_misc.mapfold_upword expect expect pw acc
       in
-      M.Ce_pword pw, ty, acc
+      M.Ce_pword pw, st, acc
 
     | Ce_equal (ce, se) ->
-      let (ce, ty), acc = clock_clock_exp env ce acc in
-      let se, acc = expect_static_exp ty se acc in
-      M.Ce_equal (ce, se), ty, acc
+      let (ce, st), acc = clock_clock_exp env ce acc in
+      let se, acc = expect_static_exp (ty_of_st st) se acc in
+      M.Ce_equal (ce, se), st, acc
 
     | Ce_iter ce ->
-      let (mce, ty), acc = clock_clock_exp env ce acc in
-      let ty, acc = on_ty loc ty (trad_clock_exp ce) acc in
-      M.Ce_iter mce, ty, acc
+      let (mce, st), acc = clock_clock_exp env ce acc in
+      M.Ce_iter mce, Pst_on (st, trad_clock_exp ce), acc
   in
   (
     {
       M.ce_desc = ced;
       M.ce_loc = loc;
-      M.ce_info = annotate_exp ce.ce_info ty;
+      M.ce_info = annotate_clock_exp ce.ce_info st;
     },
-    ty
+    st
   ),
   acc
 
-and expect_clock_exp env expected_ty ce (ctx, constrs) =
-  let (ce, actual_ty), (ctx, constrs) = clock_clock_exp env ce (ctx, constrs) in
-  ce, (ctx, unify ce.M.ce_loc expected_ty actual_ty constrs)
+and expect_clock_exp env expected_st ce (ctx, constrs) =
+  let (ce, actual_st), (ctx, constrs) = clock_clock_exp env ce (ctx, constrs) in
+  let cstrs =
+    unify ce.M.ce_loc (ty_of_st expected_st) (ty_of_st actual_st) constrs
+  in
+  ce, (ctx, cstrs)
 
 and clock_static_exp se =
-  let ty = fresh_ty () in
+  let st = fresh_st () in
   {
     M.se_desc = se.se_desc;
     M.se_loc = se.se_loc;
-    M.se_info = annotate_exp se.se_info ty;
+    M.se_info = annotate_clock_exp se.se_info st;
   },
-  ty
+  st
 
 and expect_static_exp expected_ty se (ctx, constrs) =
-  let se, actual_ty = clock_static_exp se in
-  se, (ctx, unify se.M.se_loc expected_ty actual_ty constrs)
+  let se, actual_st = clock_static_exp se in
+  se, (ctx, unify se.M.se_loc expected_ty (ty_of_st actual_st) constrs)
 
 and clock_exp env e acc =
   let loc = e.e_loc in
@@ -461,31 +464,34 @@ and clock_exp env e acc =
   | E_when (e, ce) ->
     let cce = trad_clock_exp ce in
     let (e, ty), acc = clock_exp env e acc in
-    let ce, acc = expect_clock_exp env ty ce acc in
-    let ty, acc = on_ty loc ty cce acc in
-    M.E_when (e, ce), ty, acc
+    let st, acc = st_of_ty loc ty acc in
+    let ce, acc = expect_clock_exp env st ce acc in
+    M.E_when (e, ce), ty_of_st (Pst_on (st, cce)), acc
 
   | E_split (ce, e, ec_l) ->
     let cce = trad_clock_exp ce in
     let (e, ty), acc = clock_exp env e acc in
-    let ce, acc = expect_clock_exp env ty ce acc in
+    let st, acc = st_of_ty loc ty acc in
+    let ce, acc = expect_clock_exp env st ce acc in
     let ty_l, acc = Utils.mapfold (sampled_ty loc ty cce) ec_l acc in
     M.E_split (ce, e, ec_l), prod_ty ty_l, acc
 
   | E_bmerge (ce, e1, e2) ->
     let cce = trad_clock_exp ce in
-    let (ce, ty), acc = clock_clock_exp env ce acc in
+    let (ce, st), acc = clock_clock_exp env ce acc in
+    let ty = ty_of_st st in
     let e1, acc =
       expect_sampled_exp env ty cce (Ast_misc.Ec_bool true) e1 acc
     in
     let e2, acc =
       expect_sampled_exp env ty cce (Ast_misc.Ec_bool false) e2 acc
     in
-    M.E_bmerge (ce, e1, e2), ty, acc
+    M.E_bmerge (ce, e1, e2), ty_of_st st, acc
 
   | E_merge (ce, c_l) ->
     let cce = trad_clock_exp ce in
-    let (ce, ty), acc = clock_clock_exp env ce acc in
+    let (ce, st), acc = clock_clock_exp env ce acc in
+    let ty = ty_of_st st in
     let c_l, acc =
       let clock_clause c acc =
         let e, acc =
@@ -500,14 +506,15 @@ and clock_exp env e acc =
       in
       Utils.mapfold clock_clause c_l acc
     in
-    M.E_merge (ce, c_l), ty, acc
+    M.E_merge (ce, c_l), ty_of_st st, acc
 
   | E_valof ce ->
-    let (ce, ty), acc = clock_clock_exp env ce acc in
-    M.E_valof ce, ty, acc
+    let (ce, st), acc = clock_clock_exp env ce acc in
+    M.E_valof ce, ty_of_st st, acc
 
   | E_clock_annot (e, cka) ->
-    let (cka, ty), acc = clock_clock_annot env loc cka acc in
+    let (cka, st), acc = clock_clock_annot env loc cka acc in
+    let ty = ty_of_st st in
     let e, acc = expect_exp env ty e acc in
     M.E_clock_annot (e, cka), ty, acc
 
@@ -549,10 +556,9 @@ and clock_clock_annot env loc cka acc =
     (M.Ca_var v, find_ck_var env v), acc
   | Ca_on (cka, ce) ->
     let cce = trad_clock_exp ce in
-    let (cka, ty), acc = clock_clock_annot env loc cka acc in
-    let ce, acc = expect_clock_exp env ty ce acc in
-    let res_ty, acc = on_ty loc ty cce acc in
-    (M.Ca_on (cka, ce), res_ty), acc
+    let (cka, st), acc = clock_clock_annot env loc cka acc in
+    let ce, acc = expect_clock_exp env st ce acc in
+    (M.Ca_on (cka, ce), Pst_on (st, cce)), acc
 
 and clock_pattern env p acc =
   let loc = p.p_loc in
@@ -568,7 +574,8 @@ and clock_pattern env p acc =
       M.P_tuple p_l, prod_ty ty_l, acc
 
     | P_clock_annot (p, cka) ->
-      let (cka, ty), acc = clock_clock_annot env loc cka acc in
+      let (cka, st), acc = clock_clock_annot env loc cka acc in
+      let ty = ty_of_st st in
       let p, acc = expect_pattern env ty p acc in
       M.P_clock_annot (p, cka), ty, acc
 

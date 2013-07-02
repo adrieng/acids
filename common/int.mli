cab753b434db9f39e7086e76327395cd8acedfa9
Adrien Guatto <adrien.guatto@laposte.net>
2013-07-02 09:06:26 +0200

    Oops, forgot .mli file

---
+++ common/int.mli
@@ -0,0 +1,62 @@
+(* Copyright (C) Adrien Guatto <adrien.guatto@laposte.net> 2013
+ *
+ * This file is part of Acid Synchrone.
+ *
+ * nsched is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * nsched is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * nsched. If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+type t
+
+val of_int : int -> t
+
+val to_int : t -> int
+
+val add : t -> t -> t
+
+val sub : t -> t -> t
+
+val mul : t -> t -> t
+
+val div : t -> t -> t
+
+val zero : t
+
+val one : t
+
+val abs : t -> t
+
+val succ : t -> t
+
+val pred : t -> t
+
+val min_int : t
+
+val max_int : t
+
+val neg : t -> t
+
+val logxor : t -> t -> t
+
+val logor : t -> t -> t
+
+val logand : t -> t -> t
+
+val lognot : t -> t
+
+val of_char : char -> t
+
+val of_string : string -> t
+
+val print : Format.formatter -> t -> unit
+
+module Env : Map.S with type key = t

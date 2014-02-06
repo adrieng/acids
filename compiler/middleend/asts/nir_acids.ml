a4a0db7c118cdc9302b6e47c05981cfbd1401648
Adrien Guatto <adrien.guatto@laposte.net>
2014-02-06 21:02:47 +0100

    Add missing API file

---
+++ compiler/middleend/asts/nir_acids.ml
@@ -0,0 +1,28 @@
+(* Copyright (C) Adrien Guatto <adrien.guatto@laposte.net> 2013-2014
+ *
+ * This file is part of Acid Synchrone.
+ *
+ * nsched is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * nsched is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * nsched. If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+module Info =
+struct
+  type node_name = Names.longname
+  let print_node_name = Names.print_longname
+end
+
+module M = Nir.Make(Info)
+include M
+
+module P = Nir_printer.Make(M)
+include P
